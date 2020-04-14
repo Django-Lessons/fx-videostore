60216064497985e72d883df3802ad4992bf9673e
Eugen Ciur <eugen@django-lessons.com>
2020-04-14 16:57:17 +0200

    webpack as per bootstrap installation instructions

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'development',
+  output: {
+    filename: 'land.js',
+    path: "/demo/videostoreproj/land/static/land/js/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+    ],
+  },
+};
