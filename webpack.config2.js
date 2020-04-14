0da955da1943c6677e78443afee0426eeb66a24e
Eugen Ciur <eugen@django-lessons.com>
2020-04-14 17:18:40 +0200

    yet another option, this time

---
+++ webpack.config2.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'development',
+  output: {
+    filename: 'index.js',
+    path: "/demo/videostoreproj/land/static/land/js/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader, // inject CSS to page
+          }, 
+          {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          }, 
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }]
+        },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+  ],
+};
