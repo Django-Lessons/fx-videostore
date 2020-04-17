1778618eeb40e441ffd24c1edd39bd8ce783de0a
Eugen Ciur <eugen@django-lessons.com>
2020-04-17 07:41:11 +0200

    adding babel into the picture

---
+++ config_with_babel.js
@@ -0,0 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'development',
+  output: {
+    filename: 'index.js',
+    path: "/demo/videostoreproj/land/static/land/js/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+          ]
+        },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/index.css',
+    }),
+  ]
+};
