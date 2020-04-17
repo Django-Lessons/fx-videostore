f2f130eb4f71361aac014269d533972ad7a10e9b
Eugen Ciur <eugen@django-lessons.com>
2020-04-17 08:39:56 +0200

    pretty good Duo!

--- src/js/duo_pass.js
+++ src/js/duo_pass.js
@@ -1,7 +1,51 @@
+import $ from "jquery";
+
+
+class Password {
+    constructor(pwd, keyup_handler, context) {
+        /* pwd is jquery instance of password input*/
+        this._pwd = pwd;
+        this._pwd.on('keyup', function(event){
+            keyup_handler.apply(context, event.data);            
+        });
+    }
+
+    get text() {
+        return this._pwd.val(); 
+    }
+}
 
 
 export class DuoPass {
+
+    static get RED() {
+        return "red";
+    }
+
+    static get GREEN() {
+        return "green";
+    }
+
     constructor() {
+        this._pass1 = new Password($("#password1"), this.keyup, this);
+        this._pass2 = new Password($("#password2"), this.keyup, this); 
+    }
+
+    keyup() {
+        let text1, text2;
+
+        text1 = this._pass1.text;
+        text2 = this._pass2.text;
+
+        if (text1 != text2) {
+            this.mark(DuoPass.RED);
+        } else {
+            this.mark(DuoPass.GREEN);
+        }
+    }
+
+    mark(color) {
+        console.log(`Mark color=${color}`);
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
+import $ from 'jquery';
 import '../sass/index.scss';
 import 'bootstrap';
 
 import {DuoPass} from "./duo_pass";
 
-let duo_pass = new DuoPass();
+function is_registration() {
+    return $("form#registration").length != 0;
+}
+
+$(function() {
+    let duo_pass;
+
+    if (is_registration()) {
+        duo_pass = new DuoPass();
+    }
+});
