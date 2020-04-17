cb962e890c4387840f3c0195ab783f6ee0bd6c88
Eugen Ciur <eugen@django-lessons.com>
2020-04-17 07:53:22 +0200

    only duo pass will do

--- src/js/duo_pass.js
+++ src/js/duo_pass.js
@@ -1,2 +1,7 @@
 
 
+export class DuoPass {
+    constructor() {
+    }
+}
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 import '../sass/index.scss';
 import 'bootstrap';
+
+import {DuoPass} from "./duo_pass";
+
+let duo_pass = new DuoPass();
